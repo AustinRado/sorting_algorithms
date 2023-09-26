4cad885326c319ced0497d830c7ec145cb7d4d88
AustinRado <radoaussy@gmail.com>
2023-09-26 11:41:49 -0700

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,28 +8,26 @@
 
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int temp;
-bool swapped;
+	size_t i, j;
+	int temp;
+	bool swapped;
 
-if (size <= 1 || array == NULL)
-return;
+	if (size <= 1 || array == NULL)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = false;
-            for (j = 0; j < size - 1 - i; j++)
-            {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = true;
-                print_array(array, size);
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		if (array[j] > array[j + 1])
+		{
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			swapped = true;
+			print_array(array, size);
+		}
+	}
+	if (!swapped)
+		break;
 }
+
